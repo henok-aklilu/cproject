d2ee191804300efe2f0a0803e4e35b571625c010
BinyamMamo <binyammamo01@gmail.com>
2023-01-25 02:53:21 +0300

    update handle print

--- handle_print.c
+++ handle_print.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
+ * handle_print - situates the printin process
+ * @fmt: string formatter
+ * @args: displayed arguments
+ * @ind: pointer to index of buffer
+ * @buffer: stores the buffer
+ * @flags: format flags
+ * @width: field width
+ * @prec: output precision
+ * @size: output size
+ * Return: printed chars or -1 during failure
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list args, char buffer[],
+	int flags, int width, int prec, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -24,7 +24,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+			return (fmt_types[i].fn(args, buffer, flags, width, prec, size));
 
 	if (fmt_types[i].fmt == '\0')
 	{
